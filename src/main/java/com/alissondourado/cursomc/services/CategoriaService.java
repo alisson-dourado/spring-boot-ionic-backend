de63ae1c2585ead422d0c132e2a9a6ecb17e9d9b
Alisson Dourado <douradonascimento@hotmail.com>
2020-03-21 21:11:59 -0300

    Tratando exception endpoint /categorias/{id}

--- src/main/java/com/alissondourado/cursomc/services/CategoriaService.java
+++ src/main/java/com/alissondourado/cursomc/services/CategoriaService.java
@@ -7,15 +7,17 @@ import org.springframework.stereotype.Service;
 
 import com.alissondourado.cursomc.domain.Categoria;
 import com.alissondourado.cursomc.repositories.CategoriaRepository;
+import com.alissondourado.cursomc.services.exeptions.ObjectNotFoundException;
 
 @Service
 public class CategoriaService {
-	
+
 	@Autowired
 	private CategoriaRepository repo;
-	
+
 	public Categoria buscar(Integer id) {
 		Optional<Categoria> obj = repo.findById(id);
-		return obj.orElse(null);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));
 	}
 }
