610c8ca5a4dcae80f3be24a6aad7f8aa34e39f52
Alisson Dourado <douradonascimento@hotmail.com>
2020-05-01 22:02:20 -0300

    Implementando autorizacao

--- src/main/java/com/alissondourado/cursomc/config/SecurityConfig.java
+++ src/main/java/com/alissondourado/cursomc/config/SecurityConfig.java
@@ -19,6 +19,7 @@ import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import com.alissondourado.cursomc.security.JWTAuthenticationFilter;
+import com.alissondourado.cursomc.security.JWTAuthorizationFilter;
 import com.alissondourado.cursomc.security.utils.JWT;
 
 @Configuration
@@ -50,6 +51,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 		http.authorizeRequests().antMatchers(HttpMethod.GET, PUBLIC_MATCHERS_GET).permitAll()
 				.antMatchers(PUBLIC_MATCHERS).permitAll().anyRequest().authenticated();
 		http.addFilter(new JWTAuthenticationFilter(authenticationManager(), jwt));
+		http.addFilter(new JWTAuthorizationFilter(authenticationManager(), jwt, userDetailsService));
 		http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 	}
 	
---
+++ src/main/java/com/alissondourado/cursomc/security/JWTAuthorizationFilter.java
@@ -0,0 +1,58 @@
+package com.alissondourado.cursomc.security;
+
+import java.io.IOException;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
+
+import com.alissondourado.cursomc.security.utils.JWT;
+
+public class JWTAuthorizationFilter extends BasicAuthenticationFilter {
+
+	private JWT jwt;
+
+	private UserDetailsService userDetailsService;
+
+	public JWTAuthorizationFilter(AuthenticationManager authenticationManager, JWT jwt,
+			UserDetailsService userDetailsService) {
+		super(authenticationManager);
+		this.jwt = jwt;
+		this.userDetailsService = userDetailsService;
+	}
+
+	@Override
+	protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain chain)
+			throws IOException, ServletException {
+
+		String header = request.getHeader("Authorization");
+
+		if (header != null && header.startsWith("Bearer ")) {
+
+			UsernamePasswordAuthenticationToken auth = getAuthentication(header.substring(7));
+
+			if (auth != null) {
+				SecurityContextHolder.getContext().setAuthentication(auth);
+			}
+		}
+		chain.doFilter(request, response);
+	}
+
+	private UsernamePasswordAuthenticationToken getAuthentication(String token) {
+		if (jwt.tokenValid(token)) {
+			String username = jwt.getUsername(token);
+			UserDetails user = userDetailsService.loadUserByUsername(username);
+			return new UsernamePasswordAuthenticationToken(user, null, user.getAuthorities());
+		}
+		return null;
+	}
+
+}
